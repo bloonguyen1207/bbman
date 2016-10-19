181fbd8a2fe22252e54bbc1ded4ac64996dfb16c
Bloo Nguyen <bloonguyen1207@gmail.com>
2016-10-19 10:43:02 +0700

    Server setup

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
-var http = require('http');
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-var server = http.createServer(function(req, res) {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end('<h1>Hello World</h1>');
+app.set('port', (process.env.PORT || 2222));
+
+app.use(express.static(__dirname));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('<h1>Hello World</h1>');
 });
 
-var port = Number(process.env.PORT || 2222);
-server.listen(port);
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
