6f38478ab0fc42aca398d9f059e8ac47ae8758cc
Bloo Nguyen <bloonguyen1207@gmail.com>
2016-10-29 10:32:49 +0700

    Working on states

--- views/states/map.js
+++ views/states/map.js
@@ -1,18 +1,22 @@
 var map_text = "Map selection";
-var instruction_text = "Press space to start";
 
 var map = {
 
 	create: function() {
-		var title = game.add.text(game.world.width / 2 - 200, 80, map_text, {
-			font: '80px Calibri', 
-			fill:'#ffffff'
+
+		var bg = game.add.sprite(0, 0,'grassbg');
+
+		var title = game.add.text(game.world.centerX - 100, 10, map_text, {
+			font: '40px Calibri', 
 		});
 		
-		var instruction = game.add.text(game.world.width / 2 - 100, game.world.height - 80, instruction_text, {
+		var instruction = game.add.text(game.world.centerX - 130, game.world.height - 35, instruction_text, {
 			font: '30px Calibri',
-			fill:'#ffffff' 
 		});
+
+		instruction.alpha = 0;
+
+		game.add.tween(instruction).to( { alpha: 1 }, 800, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
 		var start_key = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
--- views/states/menu.js
+++ views/states/menu.js
@@ -1,4 +1,4 @@
-var title_text = "Fire the hole";
+// var title_text = "Fire the hole";
 var instruction_text = "Press space to proceed";
 var titleOffsetX = 55;
 var titleOffsetY = 20;
@@ -6,16 +6,23 @@ var titleOffsetY = 20;
 var menu = {
 
 	create: function() {
+		var bg = game.add.sprite(0, 0,'menubg');
+		bg.scale.setTo(0.6, 0.65);
+
         game.load.image('title','res/tilesets/tiltle_screen.png');
-		var title = game.add.text(game.world.width / 2 - 200, 80, title_text, {
-			font: '80px Calibri', 
-			fill:'#ffffff'
-		});
+
+		// var title = game.add.text(game.world.width / 2 - 200, 80, title_text, {
+		// 	font: '80px Calibri', 
+		// 	fill:'#ffffff'
+		// });
 		
-		var instruction = game.add.text(game.world.width / 2 - 100, game.world.height - 80, instruction_text, {
+		var instruction = game.add.text(game.world.centerX - 130, game.world.height - 35, instruction_text, {
 			font: '30px Calibri',
-			fill:'#ffffff' 
 		});
+
+		instruction.alpha = 0;
+
+		game.add.tween(instruction).to( { alpha: 1 }, 800, Phaser.Easing.Linear.None, true, 0, 1000, true);
 
 		var proceed_key = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
 
