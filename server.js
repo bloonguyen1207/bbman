1fa46d61d18240a162fe08dac6e11d63b4c820db
Bloo Nguyen <bloonguyen1207@gmail.com>
2016-11-20 14:13:34 +0700

    Initializing socket

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 var app = express();
 
 app.set('port', (process.env.PORT || 2222));
